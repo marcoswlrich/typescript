0324f7555f59da5475e606ab377014530c26a755
Marcos Wlrich <marcosapw.dev@gmail.com>
2022-02-10 00:47:29 -0300

    Getters e Setters

--- POO/src/app.ts
+++ POO/src/app.ts
@@ -1,11 +1,5 @@
 import { ContaBancaria } from './models/ContaBancaria';
 
 const conta1 = new ContaBancaria();
-let saldoAtual = conta1.consultar();
-console.log(`Saldo inicial: ${saldoAtual}`);
-conta1.depositar(100);
-saldoAtual = conta1.consultar();
-console.log(`Saldo atual: ${saldoAtual}`);
-conta1.sacar(80);
-saldoAtual = conta1.consultar();
-console.log(`Saldo atual: ${saldoAtual}`);
+conta1.agencia = '123-7';
+conta1.numero = '12345-6';
--- POO/src/models/ContaBancaria.ts
+++ POO/src/models/ContaBancaria.ts
@@ -1,8 +1,40 @@
 class ContaBancaria {
   private _saldo: number;
 
+  private _numero: string;
+
+  private _agencia: string;
+
   constructor() {
     this._saldo = 0;
+    this._numero = '00000-0';
+    this._agencia = '000-0';
+  }
+
+  get numero(): string {
+    return this._numero;
+  }
+
+  set numero(valor: string) {
+    const regex = /^\d{5}-\d{1}$/;
+    if (regex.test(valor)) {
+      this._numero = valor;
+    } else {
+      console.log('Numero de conta inválida...');
+    }
+  }
+
+  get agencia(): string {
+    return this._agencia;
+  }
+
+  set agencia(valor: string) {
+    const regex = /^\d{3}-\d{1}$/;
+    if (regex.test(valor)) {
+      this._agencia = valor;
+    } else {
+      console.log('Número de agência inválida...');
+    }
   }
 
   consultar(): number {
