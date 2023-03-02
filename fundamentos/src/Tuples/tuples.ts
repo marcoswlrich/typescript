01200232543112ae66b826ec4a955c2b32b71554
marcoswlrich <marcosapw.dev@gmail.com>
2023-03-02 00:06:59 -0400

    Course

---
+++ fundamentos/src/Tuples/tuples.ts
@@ -0,0 +1,5 @@
+type Drink = [string, boolean, number];
+
+const pepsi: Drink = ['brown', true, 40];
+const sprite: Drink = ['clear', true, 40];
+const tea: Drink = ['green', false, 0];
---
+++ fundamentos/src/interfaces.ts
@@ -0,0 +1,21 @@
+interface Vehicle {
+  name: string;
+  year: number;
+  broken: boolean;
+  summary(): string;
+}
+
+const oldCivic = {
+  name: 'civic',
+  year: 2000,
+  broken: true,
+  summary(): string {
+    return `Name: ${this.name}`;
+  }
+};
+
+const printSummary = (item: Vehicle): void => {
+  console.log(item.summary());
+}
+
+printSummary(oldCivic);
